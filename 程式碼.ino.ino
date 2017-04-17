69a52b7d9a2fb802dc6f8545b865e0c5af4fde08
04050042 <hank65116758a9u@gmail.com>
2017-04-17 19:19:10 +0800

    Add files via upload

---
+++ 程式碼.ino.ino
@@ -0,0 +1,33 @@
+#include <SevSeg.h>
+SevSeg sevseg;
+int Btn1,Btn2; 
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("12/04050042/04052572");
+  
+  byte numDigits = 4;
+  byte digitPins[] = {13,12,11,10};
+  byte segmentPins[] = {2, 3, 4, 5, 6, 7, 8, 9};
+  sevseg.begin(COMMON_CATHODE, numDigits, digitPins,segmentPins);
+}
+void loop()
+{
+  if(Serial.available()>0)
+  {
+      12
+       int val = Serial.parseInt();
+       if(val>=0 && val<=9999)
+       {
+         sevseg.setNumber(val,0);
+         Serial.println(val);
+
+       }
+       else
+         Serial.println("range error");  
+  }
+  else
+    sevseg.refreshDisplay();
+}
+
